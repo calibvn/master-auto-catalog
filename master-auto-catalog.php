daafea8eb1dcfee6c63d0eb8fe8ef1ab4ab33898
calibvn <askar.ramirov@mail.ru>
2026-05-19 21:44:23 +0500

    test2

--- master-auto-catalog.php
+++ master-auto-catalog.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Мастер настроек каталога авто
  * Description: Единый мастер для VIN-импорта, логов поиска, синхронизации, Google Indexing и криптоплатежей каталога авто.
- * Version: 1.0.10
+ * Version: 1.0.11
  * Author: AskarTech
  */
 
